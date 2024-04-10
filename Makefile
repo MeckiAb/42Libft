311abe962f7ed90eb30041f24c3bcacd2d3ecc22
Mecki Abderrahaman Elena <labderra@student.42malaga.com>
2024-04-10 16:47:58 +0200

    ft_strlcat

--- Makefile
+++ Makefile
@@ -17,7 +17,9 @@ ft_strlen.c \
 ft_memset.c \
 ft_bzero.c \
 ft_memcpy.c \
-ft_memmove.c
+ft_memmove.c \
+ft_strlcpy.c \
+ft_strlcat.c
 
 # Archivos objeto
 #OBJ := $(patsubst %.c,%.o,$(FT_FILES))
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: labderra <labderra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 09:00:09 by labderra          #+#    #+#             */
-/*   Updated: 2024/04/09 17:55:37 by labderra         ###   ########.fr       */
+/*   Updated: 2024/04/10 13:50:56 by labderra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,8 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
 
 #endif
