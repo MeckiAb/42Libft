3fd30a39b55fa91c96f4234f7f77c15a18f0f3d6
labderra <labderra@student.42malaga.com>
2024-05-17 10:57:30 +0200

    refactoring ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,14 +14,11 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
+	while (n-- > 0)
 	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return ((void *)&s[i]);
-		i++;
+		if (*(unsigned char *)s == (unsigned char)c)
+			return ((void *)s);
+		s++;
 	}
 	return (NULL);
 }
